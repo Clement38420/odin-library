2a7d18a0ed15f805c4c029a83020bf588b72c97c
clement <c.charbo38@gmail.com>
2024-12-26 21:19:09 +0100

    ADD THE READ BOOK PROPERTY
    Add a read property to each book and a button in its card to invert it

--- script.js
+++ script.js
@@ -4,14 +4,24 @@ const addBookForm = document.querySelector("form");
 
 const myLibrary = [];
 
-function Book(author, title, pages) {
+function Book(author, title, pages, read) {
     this.author = author;
     this.title = title;
     this.pages = pages;
+    this.read = read;
 }
 
-function addBookToLibrary(library, author, title, pages) {
-    library.push(new Book(author, title, pages));
+Book.prototype.toggleRead = function () {
+    this.read = !this.read;
+}
+
+function addBookToLibrary(library, author, title, pages, read) {
+    library.push(new Book(author, title, pages, read));
+}
+
+function readBook(e) {
+    myLibrary[parseInt(e.target.parentElement.parentElement.dataset.id)].toggleRead();
+    displayBooks();
 }
 
 function createBookCard(book) {
@@ -20,16 +30,34 @@ function createBookCard(book) {
     bookCard.dataset.id = myLibrary.indexOf(book).toString();
 
     for (let bookProperty in book) {
-        const span = document.createElement("span");
-        span.innerText = book[bookProperty];
-        bookCard.appendChild(span);
+        if (typeof book[bookProperty] !== "function") {
+            if (bookProperty === "read") {
+                const span = document.createElement("span");
+                span.innerText = book.read ? "Already read" : "Not read yet";
+                bookCard.appendChild(span);
+            } else {
+                const span = document.createElement("span");
+                span.innerText = book[bookProperty];
+                bookCard.appendChild(span);
+            }
+        }
     }
+
+    const buttonsContainer = document.createElement("div");
 
     const deleteBtn = document.createElement("button");
     deleteBtn.classList.add("btn", "cancel", "delete-book-btn");
     deleteBtn.innerText = "Delete";
     deleteBtn.addEventListener("click", deleteBook);
-    bookCard.appendChild(deleteBtn)
+    buttonsContainer.appendChild(deleteBtn)
+
+    const readBtn = document.createElement("button");
+    readBtn.classList.add("btn", "read-book-btn");
+    readBtn.innerText = "Read";
+    readBtn.addEventListener("click", readBook);
+    buttonsContainer.appendChild(readBtn)
+
+    bookCard.appendChild(buttonsContainer)
 
     booksContainer.appendChild(bookCard);
 }
@@ -55,7 +83,8 @@ function closeAddBookDialog() {
 
 function confirmAddBookDialog(e) {
     const formData = Object.fromEntries(new FormData(addBookForm));
-    addBookToLibrary(myLibrary, formData.author, formData.title, formData.pages);
+    console.log(formData.read);
+    addBookToLibrary(myLibrary, formData.author, formData.title, formData.pages, (formData.read === "on") );
     displayBooks();
     addBookForm.reset();
     addBookDialog.close();
@@ -63,6 +92,6 @@ function confirmAddBookDialog(e) {
 }
 
 function deleteBook(e) {
-    myLibrary.splice(parseInt(e.target.parentElement.dataset.id), 1);
+    myLibrary.splice(parseInt(e.target.parentElement.parentElement.dataset.id), 1);
     displayBooks();
 }
